9a4caeb5877d893ca587b0893e43699c236b9f5e
Varun2101 <iyengar.varun@gmail.com>
2021-01-13 16:44:01 +0800

    Changed text field limits

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
 application = Flask(__name__)
-defText = "Input will be truncated to 100 characters..."
+defText1 = "Input will be truncated to 1000 characters..."
+defText2 = "Input will be truncated to 100 characters..."
 
 @application.route("/", methods=['GET'])
 def home():
-	return render_template("home.html", occurrences=0, defText=defText, defKey=defText)
+	return render_template("home.html", occurrences=0, defText=defText1, defKey=defText2)
 
 @application.route("/", methods=['GET', 'POST'])
 def countOccurrences():
