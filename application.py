7a167bec6f20f9db86188021582d364ce9f644a2
Varun2101 <iyengar.varun@gmail.com>
2021-01-17 22:32:45 +0800

    Added comments

--- application.py
+++ application.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template, request
+
+# Note: for deploying to Elastic Beanstalk, Flask app must be named "application"
 application = Flask(__name__)
+
 defText1 = "Input will be truncated to 1000 characters..."
 defText2 = "Input will be truncated to 100 characters..."
 
+# Initial display when no text has been typed.
+# Get request so that default text can be displayed
 @application.route("/", methods=['GET'])
 def home():
 	return render_template("home.html", occurrences=0, defText=defText1, defKey=defText2)
 
+# Switches to this route when a post request is made
+# Get request still needed to display results and keep the input text unchanged on reload
 @application.route("/", methods=['GET', 'POST'])
 def countOccurrences():
 	text = request.form["text"]
 	key = request.form["key"]
-	text = text[:100]
+	# truncating inputs to prevent maliciously long inputs that require large amounts of computation
+	text = text[:1000]
 	key = key[:100]
 	if text == "" or key == "":
 		occ = 0
@@ -19,5 +27,7 @@ def countOccurrences():
 		occ = text.count(key)
 	return render_template("home.html", occurrences=occ, defText=text, defKey=key)
 
+# Note: the .py file containing the main function (and entrypoint to the app) must be called "application.py"
 if __name__ == '__main__':
-	application.run(debug=True)
+	application.debug = True  # remember to comment this out before deploying on production server
+	application.run()
